b1eca01af5fa31b3b4fd82d026dcac3a7acd09f0
arvirabago <rabago.arvilyzadessa@gmail.com>
2024-04-29 23:56:17 +0800

    contact form js fixed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,67 +1,82 @@
-import React, { Component } from 'react';
 import PhoneInput from 'react-phone-number-input';
 import 'react-phone-number-input/style.css';
 import { nanoid } from 'nanoid';
 import css from './ContactForm.module.css';
+import { useState } from 'react';
+import { addContact } from 'reduxPhoneBook/phoneBookSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectContacts } from 'reduxPhoneBook/selectors';
 
-
-
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const handleChangeName = event => {
+    setName(event.target.value);
   };
 
-  handleChange = value => {
-    this.setState({
-      number: value,
-    });
+  const handleChangeNumber = value => {
+    setNumber(value);
+  };
+  const checkContactExist = name => {
+    return contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
   };
 
-  handleAddContact = () => {
-    const { addContact } = this.props;
-    const { name, number } = this.state;
+  const handleAddContact = event => {
+    event.preventDefault();
 
-    if (name.trim() === '' || number.trim() === '') {
+    if (!name.trim() || !number.trim()) {
       alert('Please enter both name and phone number.');
       return;
     }
 
-    addContact({ id: nanoid(), name, number });
-    this.setState({
-      name: '',
-      number: '',
-    });
+    if (name.trim() === '') {
+      alert('Please enter a name.');
+      return;
+    }
+
+    if (checkContactExist(name)) {
+      alert(
+        'Contact with this name already exists. Please enter a different name.'
+      );
+      return;
+    }
+
+    dispatch(addContact({ id: nanoid(), name, number }));
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <div className={css.contactForm}>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я]+)*$"
-          title="Please enter a valid name"
-          required
-          value={this.state.name}
-          onChange={e => this.setState({ name: e.target.value })}
-          placeholder="Enter name"
-          className={css.input}
-        />
+  return (
+    <form className={css.contactForm} onSubmit={handleAddContact}>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Please enter a valid name"
+        required
+        value={name}
+        onChange={handleChangeName}
+        placeholder="Enter name"
+        className={css.input}
+      />
 
-        <PhoneInput
-          placeholder="Enter phone number"
-          value={this.state.number}
-          onChange={this.handleChange}
-          defaultCountry="PH"
-          className={css.customPhoneInput}
-          limitMaxLength
-        />
+      <PhoneInput
+        placeholder="Enter phone number"
+        value={number}
+        onChange={handleChangeNumber}
+        defaultCountry="PH"
+        className={css.customPhoneInput}
+        limitMaxLength
+        name="number"
+      />
 
-        <button className={css.button} onClick={this.handleAddContact}>
-          Add Contact
-        </button>
-      </div>
-    );
-  }
-}
+      <button className={css.button} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
+};
